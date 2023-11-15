156b2cb973fb2a92a22853d70818f0e334dfd31c
prasanna-ezhilmurugan <prasanna.ezhilmurugan@gmail.com>
2023-11-15 22:44:02 +0530

    Implement HOOk stucture

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,42 +1,65 @@
-import React from "react";
+import React, { useReducer } from "react";
 import DigitButton from "./DigitButton";
-import OperatorButton from "./OperatorButton"
+import OperatorButton from "./OperatorButton";
 
+export const ACTIONS = {
+  ADD_DIGIT: "add-digit",
+  CHOOSE_OPERATION: "choose-operation",
+  CLEAR: "clear",
+  DELELTE_DIGIT: "delete-digit",
+  EVALUATE: "evaluate",
+};
+
+function reducer(state, {type, payload}) {
+  switch(type){
+    case ACTIONS.ADD_DIGIT:
+      return {
+        ...state  
+      }
+  }
+}
 function App() {
+  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(
+    reducer,
+    {}
+  );
   return (
-    <div className="calculator-grid grid grid-cols-4 max-w-md bg-slate-700 pl-3 rounded-3xl" >
-
-        <div className="output col-span-4 h-48 flex flex-col justify-end items-end">
-            <div className="previous-operand text-white text-lg font-extrabold rounded-full px-5 py-2.5 text-center me-2 mb-2">{123123}</div>
-            <div className="current-operand text-white text-2xl font-extrabold rounded-full px-5 py-2.5 text-center me-2 mb-2">{123123}</div>
+    <div className="calculator-grid grid grid-cols-4 max-w-md bg-slate-700 pl-3 rounded-3xl">
+      <div className="output col-span-4 h-48 flex flex-col justify-end items-end">
+        <div className="previous-operand text-white text-lg font-extrabold rounded-full px-5 py-2.5 text-center me-2 mb-2">
+          {currentOperand}
         </div>
+        <div className="current-operand text-white text-2xl font-extrabold rounded-full px-5 py-2.5 text-center me-2 mb-2">
+          {previousOperand}
+        </div>
+      </div>
 
-        <OperatorButton operator = {"AC"} />
-        <OperatorButton operator = {"DEL"} />
-        <OperatorButton operator = {"/"} />
+      <button className="col-span-2">AC</button>
+      <button>DEL</button>
+      <OperatorButton operator={"/"} />
 
-        <DigitButton digit={7} />
-        <DigitButton digit={8} />
-        <DigitButton digit={9} />
+      <DigitButton digit={7} dispatch={dispatch} />
+      <DigitButton digit={8} dispatch={dispatch} />
+      <DigitButton digit={9} dispatch={dispatch} />
 
-        <OperatorButton operator = {"*"} />
+      <OperatorButton operator={"*"} />
 
-        <DigitButton digit={4} />
-        <DigitButton digit={5} />
-        <DigitButton digit={6} />
+      <DigitButton digit={4} dispatch={dispatch} />
+      <DigitButton digit={5} dispatch={dispatch} />
+      <DigitButton digit={6} dispatch={dispatch} />
 
-        <OperatorButton operator = {"-"} />
+      <OperatorButton operator={"-"} />
 
-        <DigitButton digit={1} />
-        <DigitButton digit={2} />
-        <DigitButton digit={3} />
+      <DigitButton digit={1} dispatch={dispatch} />
+      <DigitButton digit={2} dispatch={dispatch} />
+      <DigitButton digit={3} dispatch={dispatch} />
 
-        <OperatorButton operator = {"+"} />
+      <OperatorButton operator={"+"} />
 
-        <DigitButton digit={0} />
+      <DigitButton digit={0} dispatch={dispatch} />
 
-        <OperatorButton operator = {"."} />
-        <OperatorButton operator = {"="} />
+      <OperatorButton operator={"."} />
+      <OperatorButton operator={"="} />
     </div>
   );
 }
--- src/components/DigitButton.jsx
+++ src/components/DigitButton.jsx
@@ -5,6 +5,9 @@ function DigitButton(props) {
     <button
       type="button"
       className="text-white bg-blue-700 hover:bg-blue-900 focus:outline-none focus:ring-4 focus:ring-blue-300 font-extrabold rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      // onClick={(prevValue) =>{
+      //   props.dispatch({...prevValue, currentOperation=props.digit})
+      // }}
     >
       {props.digit}
     </button>
